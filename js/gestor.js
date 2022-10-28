8a0fee01bd0aa48385bf29990d51dddcb3f61cf4
mariocano23 <mariocano.alu@iespacomolla.es>
2022-10-28 19:41:58 +0200

    destacado

--- js/gestor.js
+++ js/gestor.js
@@ -15,6 +15,27 @@ window.onload = () => {
 
     var tarea;
 
+    document.addEventListener("dragenter", function (evento) {//Evento que pone un estilo para indicar sobre donde vas a soltar la tarea.
+        if (evento.target.id=="pendientes"||evento.target.id=="acabadas"||evento.target.classList.contains("tarea")) {
+            evento.target.classList.add("destacado");
+        }  
+    });
+
+    document.addEventListener("dragleave", function (evento) {//Evento que quita el estilo de destacado al dejar de tener el elemento arrastrado encima.
+        if (evento.target.id=="pendientes"||evento.target.id=="acabadas"||evento.target.classList.contains("tarea")) {
+            evento.target.classList.remove("destacado");
+        }
+        
+    });
+
+    document.addEventListener("dragend", function (evento) {//Evento que quita el estilo destacado al soltar el elemento.
+            evento.target.parentNode.classList.remove("destacado");
+            evento.target.classList.remove("destacado");
+    });
+
+
+//Eventos para poder arrastarar y soltar las tareas.
+
     document.addEventListener(
         "dragstart",
         function(evento){
@@ -39,7 +60,7 @@ window.onload = () => {
                 if(evento.target.id=="acabadas"&&tarea.className=="tarea"){
                     acabar(tarea.id);
                 }else{
-                    if((evento.target.className=="tarea"&&tarea.className=="tarea")){
+                    if((evento.target.className=="tarea"&&tarea.className=="tarea")){ //Comprobación para poder añadir una tarea pendienta después de otra al soltarla sobre ella.
                         insertAfter(tarea,evento.target);
                     }
                 }
